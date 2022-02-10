2e762105a01def2305bf6a982fb77371e4b2f6b9
biesbron <60372551+biesbron@users.noreply.github.com>
2022-02-10 17:47:33 -0500

    Create sample1_ipc.c

---
+++ lab5/sample1_ipc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+#define FOO 4096
+
+int main () {
+   int shmId;
+   char *shmPtr;
+   struct shmid_ds info;
+
+   if ((shmId = shmget (IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
+      perror ("i can't get no..\n");
+      exit (1);
+   }
+   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
+      perror ("can't attach\n");
+      exit (1);
+   }
+   printf ("Shared Memory ID: [%d]\n", shmId);
+   pause();
+
+   if (shmdt (shmPtr) < 0) {
+      perror ("just can't let go\n");
+      exit (1);
+   }
+   if ( shmctl(shmId, IPC_STAT, &info) < 0) {
+      perror ("can't deallocate\n");
+      exit(1);
+   }
+
+   printf("Shared Memory Size: [%ld]\n", info.shm_segsz);
+
+   return 0;
+}
