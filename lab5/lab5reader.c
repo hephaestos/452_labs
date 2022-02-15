8bfb1af8c025754f7cdfce8b715b1d236c82ea8f
biesbron <60372551+biesbron@users.noreply.github.com>
2022-02-15 18:33:36 -0500

    Update lab5reader.c

--- lab5/lab5reader.c
+++ lab5/lab5reader.c
@@ -9,6 +9,7 @@
 #include <string.h>
 
 #define FOO 4096
+#define MAXMESSAGESIZE 16
 
 void sig_handler(int);
 
@@ -16,25 +17,28 @@ int main() {
 	signal(SIGINT, sig_handler);
     int shmId;
     char *shmPtr;
+    char *turnPtr;
     int keyId = 70;
-	key_t shmKey;
-	char readString[16];
+	key_t shmKey = ftok("shmtext", keyId);
+	char readString[MAXMESSAGESIZE];
+
+    if ((shmId = shmget(shmKey, FOO, 0666)) < 0) {
+		perror ("Can't get Shared Memory ID\n");
+		exit(1);
+	}
+    if ((turnPtr = shmat(shmId, NULL, 0)) == (char*) -1) {
+		perror ("Can't attach to Shared Memory\n");
+        exit (1);
+    }
+    shmPtr = turnPtr + sizeof(int);
 
 	do {
-		shmKey = ftok("shmtext", keyId);
-		sleep(1);
-		if ((shmId = shmget(shmKey, FOO, 0666)) < 0) {
-			perror ("Can't get Shared Memory ID\n");
-			exit(1);
-		}
-		if ((shmPtr = shmat(shmId, NULL, 0)) == (char*) -1) {
-			perror ("Can't attach to Shared Memory\n");
-            exit (1);
-		}
-		if (strcmp(readString, shmPtr) != 0) {
-			printf("String [%s] read from Shared Memory\n", shmPtr);
-		}
-		strcpy(readString, shmPtr);
+        //if() //check if the turn pointer is the shared address we need
+        strcpy(readString, shmPtr);
+		printf("String [%s] read from Shared Memory\n", readString);
+        shmPtr = shmPtr + MAXMESSAGESIZE;
+        //sleep(2);
+        //}
 	} while(1);
 
 	return 0;
