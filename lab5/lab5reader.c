cec78f4a88b89dd9d63b8184a95617919b0e78a4
biesbron <60372551+biesbron@users.noreply.github.com>
2022-02-16 19:27:37 -0500

    Update lab5reader.c

--- lab5/lab5reader.c
+++ lab5/lab5reader.c
@@ -13,11 +13,12 @@
 
 void sig_handler(int);
 
+int shmId;
+char *shmTurnPtr;
+
 int main() {
 	signal(SIGINT, sig_handler);
-    int shmId;
     char *shmPtr;
-    char *shmTurnPtr;
     int keyId = 70;
 	key_t shmKey = ftok("shmtext", keyId);
 	char readString[MAXMESSAGESIZE];
@@ -36,7 +37,7 @@ int main() {
 
 	do {
         if(*shmTurnPtr == turn) { //check if turns match
-            printf("Writer turn [%d]\nReader turn[%d]\n", *shmTurnPtr, turn);
+            printf("Writer turn [%d] - Reader turn[%d]\n", *shmTurnPtr, turn);
             strcpy(readString, shmPtr);
 		    printf("String [%s] read from Shared Memory\n", readString);
             shmPtr = shmPtr + MAXMESSAGESIZE;
@@ -44,12 +45,12 @@ int main() {
             if (turn >= 127) {
                 turn = 0;
             }
-            // printf("LIMIT: [%d]\n", limiter);
-            // if (limiter >= 4095) {
-            //     printf("limit hit!\n");
-            //     limiter = sizeof(int);
-            //     shmPtr = shmTurnPtr + sizeof(int);
-            // }   
+            printf("LIMIT: [%d]\n", limiter);
+            if (limiter >= 4096) {
+                printf("limit hit!\n");
+                limiter = sizeof(int);
+                shmPtr = shmTurnPtr + sizeof(int);
+            }
             turn++;          
             //sleep(1);
         }
@@ -60,6 +61,10 @@ int main() {
 void sig_handler(int sig_num) {
     if(sig_num == SIGINT) {
         puts("\nClosing Reader Program! Shutting down gracefully.\n");
-    exit(0);
+        if (shmdt (shmTurnPtr) < 0) { //dettaching shared memory
+            perror ("Can't deattach to Shared Memory\n");
+            exit (1);
+        }
+        exit(0);
     }
 }
