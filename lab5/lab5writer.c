a7735d9f471e2e7c3e25780a40b4ed5309d4324f
biesbron <60372551+biesbron@users.noreply.github.com>
2022-02-16 19:27:08 -0500

    Update lab5writer.c

--- lab5/lab5writer.c
+++ lab5/lab5writer.c
@@ -13,17 +13,16 @@
 
 void sig_handler(int);
 
+int shmId;
+char *shmTurnPtr;
+
 int main () {
     signal(SIGINT, sig_handler);
-    int shmId;
     char *shmPtr;
-    char *shmTurnPtr;
     int keyId = 70;
     key_t shmKey = ftok("shmtext", keyId);
     char enteredString[MAXMESSAGESIZE];
     int limiter = sizeof(int);
-
-    //printf("Limit Size = %d\n",limiter);
 
     if ((shmId = shmget (shmKey, FOO, 0666|IPC_CREAT)) < 0) {
         perror ("Can't get Shared Memory ID\n");
@@ -38,18 +37,23 @@ int main () {
     *shmTurnPtr = 0;
 
     do {
+        if (*shmTurnPtr >= 127) {
+            *shmTurnPtr = 0;
+        }
+        if (limiter >= 4096) {
+            printf("limit hit!\n");
+            limiter = sizeof(int);
+            shmPtr = shmTurnPtr + sizeof(int);
+        }
         puts("Enter a String");
         fgets(enteredString, MAXMESSAGESIZE, stdin);
         enteredString[strcspn(enteredString, "\n")] = 0;
         strcpy(shmPtr, enteredString);
-        printf("Writer turn [%d]\n", *shmTurnPtr+1);
+        //printf("Writer turn [%d]\n", *shmTurnPtr+1);
         printf("String [%s] written to Shared Memory\n", shmPtr);
         shmPtr = shmPtr + MAXMESSAGESIZE; //incrementing the shared address by size of maxsizse
-        limiter = limiter + MAXMESSAGESIZE;
-        if (*shmTurnPtr >= 126) {
-            *shmTurnPtr = 0;
-        }
-        // printf("LIMIT: [%d]\n", limiter);
+        limiter = (limiter + MAXMESSAGESIZE);
+        printf("LIMIT: [%d]\n", limiter);
         // if (limiter >= 4095) {
         //     printf("limit hit!\n");
         //     limiter = sizeof(int);
@@ -62,6 +66,14 @@ int main () {
 void sig_handler(int sig_num) {
     if(sig_num == SIGINT) {
         puts("\nClosing Writer Program! Shutting down gracefully.\n");
-    exit(0);
+        if (shmdt (shmTurnPtr) < 0) { //dettaching shared memory
+            perror ("Can't deattach to Shared Memory\n");
+            exit (1);
+        }
+        if (shmctl (shmId, IPC_RMID, 0) < 0) { //deallocating
+            perror ("Can't deallocate the Shared Memory\n");
+            exit(1);
+        }
+        exit(0);
     }
 }
