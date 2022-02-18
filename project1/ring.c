fa23403a7c64910ce263c49a19773765cb1c7c41
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-18 12:29:04 -0500

    use dup2 to set up pipes between processes

--- project1/ring.c
+++ project1/ring.c
@@ -4,12 +4,15 @@
 #include <signal.h>
 #include <string.h>
 
+#define WRITE 1
+#define READ 0
+
 void sig_handler(int, siginfo_t*, void*);
 
 int main() {
     pid_t *child_pids;
+    int* *pipes;
     char input[32];
-    int fd[2];
     int n_processes;
 
     printf("How many processes should be created: ");
@@ -17,16 +20,15 @@ int main() {
     if(!(n_processes = atoi(input))) {
         perror("Invalid input\n");
         exit(1);
-    }    
+    }
 
-    child_pids = malloc(sizeof(pid_t) * n_processes);
+    child_pids = malloc(sizeof(*child_pids) * n_processes);
+    pipes = malloc(sizeof(*pipes) * n_processes);
     printf("Spawning %d processes\n", atoi(input));
     for(int i=0; i<n_processes; ++i) {
-        if(pipe(fd) < 0) {
-            perror("pipe failed");
-            exit(1);
-        }
         if ((child_pids[i] = fork()) < 0) {
+            dup2(pipes[i+1][WRITE], STDOUT_FILENO);
+            dup2(pipes[i][READ], STDIN_FILENO);
             perror ("fork failed");
             exit(1);
         } else if (!child_pids[i]) {
