b5706cef939176effd30c223fe56039a09db05d3
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-25 14:31:49 -0500

    don't use dup2 and get inputs from user

--- project1/ring.c
+++ project1/ring.c
@@ -7,23 +7,46 @@
 
 #define WRITE 1
 #define READ 0
+#define MAX_MESSAGE_SIZE 999
 
 bool time_to_die = false;
 
 void sig_handler(int, siginfo_t*, void*);
 
+void blue_fputs(char* s, FILE* stream) {
+    fputs("\033[0;34m", stream);
+    fputs(s, stream);
+    fputs("\033[0m", stream);
+}
+
+void fail(char* error, int status) {
+    puts("\033[0;31m");
+    perror(error);
+    puts("\033[0m");
+    exit(status);
+}
+
 int main() {
     pid_t *child_pids;
     int* *pipes;
-    char input[32];
+    char number_of_processes[3];
+    char destination_process[3];
+    char message[MAX_MESSAGE_SIZE];
     int n_processes;
 
-    printf("How many processes should be created: ");
+    struct apple {
+        int destination;
+        char *message;
+    };
+
+    blue_fputs("How many processes should be created: ", stdout);
     // Get the number of processes from the user
-    fgets(input, 32, stdin);
-    if(!(n_processes = atoi(input))) {
-        perror("Invalid input\n");
-        exit(1);
+    fgets(number_of_processes, 3, stdin);
+    if(!(n_processes = atoi(number_of_processes))) {
+        fail("Invalid input", 1);
+    }
+    if(n_processes < 0 || n_processes > 999) {
+        fail("Please enter a number 1-999", 1);
     }
 
     // Dynamically allocate arrays for pipes and pids based on input
@@ -31,55 +54,77 @@ int main() {
     pipes = malloc(sizeof(*pipes) * n_processes);
 
     // Set up all the pipes
+    printf("Creating %d pipes...\n", n_processes);
     for(int i=0; i<n_processes; ++i) {
         // Create new pipe and put it in the array
         int fd[2];
         if(pipe(fd) < 0) {
-            perror("Failed to create pipe");
-            exit(1);
+            fail("Failed to create pipe", 1);
         }
         pipes[i] = fd; 
     }
 
     // Create all the processes and set their stdout and stdin
     // to the appropriate pipes
-    printf("Spawning %d processes\n", atoi(input));
+    printf("Forking %d processes...\n", n_processes);
     for(int i=0; i<n_processes; ++i) {
         // Fork a child process and exit on error
         if((child_pids[i] = fork()) < 0) {
-            perror ("fork failed");
-            exit(1);
+            fail("fork failed", 1);
         // Set up each child process
         } else if (!child_pids[i]) {
-            printf("Created node #%d (pid=%d)\n", i, getpid());
+            //printf("Created node #%d (pid=%d)\n", i, getpid());
             // The first process should write to the second but read from the last
+            int read_fd, write_fd;
             if(i==0) {
-                printf("\tConnecting pipes...\n\t\tWRITE: %d\n\t\tREAD: %d\n", i+1, 0);
-                dup2(pipes[i+1][WRITE], STDOUT_FILENO);
-                dup2(pipes[0][READ], STDIN_FILENO);
+                //printf("Node %d: \n\tRead from pipe: %d\n\tWrite to pipe: %d\n", i, 0, i+1);
+                read_fd = 0;
+                write_fd = i+1;
             // The last process should write to the first and read from the second to last
             } else if(i==n_processes-1) {
-                printf("\tConnecting pipes...\n\t\tWRITE: %d\n\t\tREAD: %d\n", 0, i);
-                dup2(pipes[0][WRITE], STDOUT_FILENO);
-                dup2(pipes[i][READ], STDIN_FILENO);
+                //printf("Node %d: \n\tRead from pipe: %d\n\tWrite to pipe: %d\n", i, i, 0);
+                read_fd = i;
+                write_fd = 0;
             // Each process should share a pipe with the next process and the previous
             } else {
-                printf("\tConnecting pipes...\n\t\tWRITE: %d\n\t\tREAD: %d\n", i+1, i);
-                dup2(pipes[i+1][WRITE], STDOUT_FILENO);
-                dup2(pipes[i][READ], STDIN_FILENO);
+                //printf("Node %d: \n\tRead from pipe: %d\n\tWrite to pipe: %d\n", i, i, i+1);
+                read_fd = i;
+                write_fd = i+1;
             }
-            break; // Don't keep the loop running in the child process
+            exit(0); // Don't keep the loop running in the child process
         // Parent process
         } else {
         }
     }
-    printf("Hi I'm the parent(pid=%d)", getpid());
-    printf("Done setting up child processes. Creating signal handler...\n");
+    puts("Done setting up child processes. Creating signal handler...");
     // Set up SIGINT handler
     struct sigaction new_action;
     new_action.sa_sigaction = &sig_handler;
     new_action.sa_flags = SA_SIGINFO;
     sigaction(SIGINT, &new_action, NULL);
+    
+    struct apple the_apple;
+
+    blue_fputs("Destination node: ", stdout);
+    fgets(destination_process, 3, stdin);
+
+    if(!(the_apple.destination = atoi(destination_process))) {
+        fail("Destination process must be a valid number", 1);
+    }
+    if(the_apple.destination > n_processes) {
+        fail("Invalid destination process", 1);
+    }
+
+    blue_fputs("Message: ", stdout);
+    fgets(message, MAX_MESSAGE_SIZE, stdin);
+    the_apple.message = malloc(MAX_MESSAGE_SIZE * sizeof(*the_apple.message));
+    strcpy(the_apple.message, message);
+
+    fputs("Sending message to node ", stdout);
+    fputs(destination_process, stdout);
+    fputs("Message: ", stdout);
+    fputs(the_apple.message, stdout);
+
     while(!time_to_die) {
         pause();
     }
