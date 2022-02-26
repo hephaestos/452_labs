5ea228045239d57d843578a3c13c36a31e97cdb7
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-25 22:03:53 -0500

    we passed the f*cking appple

--- project1/ring.c
+++ project1/ring.c
@@ -28,6 +28,7 @@ void fail(char* error, int status) {
 
 int main() {
     pid_t *child_pids;
+    pid_t parent_pid;
     int* *pipes;
     char number_of_processes[3];
     char destination_process[3];
@@ -55,6 +56,7 @@ int main() {
     pipes = malloc(sizeof(*pipes) * n_processes);
 
     // Set up all the pipes
+    parent_pid = getpid();
     printf("Creating %d pipes...\n", n_processes);
     for(int i=0; i<n_processes; ++i) {
         // Create new pipe and put it in the array
@@ -68,50 +70,35 @@ int main() {
     // Create all the processes and set their stdout and stdin
     // to the appropriate pipes
     printf("Forking %d processes...\n", n_processes);
-    for(int i=0; i<n_processes; ++i) {
+    for(int i=2; i<=n_processes; ++i) {
         // Fork a child process and exit on error
-        if((child_pids[i] = fork()) < 0) {
+        if((child_pids[i-2] = fork()) < 0) {
             fail("fork failed", 1);
         // Set up each child process
-        } else if (!child_pids[i]) {
+        } else if (!child_pids[i-2]) {
             //printf("Created node #%d (pid=%d)\n", i, getpid());
             // The first process should write to the second but read from the last
-            int read_fd, write_fd;
-            if(i==0) {
-                //printf("Node %d: \n\tRead from pipe: %d\n\tWrite to pipe: %d\n", i, 0, i+1);
-                read_fd = 0;
-                write_fd = i+1;
-            // The last process should write to the first and read from the second to last
-            } else if(i==n_processes-1) {
-                //printf("Node %d: \n\tRead from pipe: %d\n\tWrite to pipe: %d\n", i, i, 0);
-                read_fd = i;
-                write_fd = 0;
-            // Each process should share a pipe with the next process and the previous
-            } else {
-                //printf("Node %d: \n\tRead from pipe: %d\n\tWrite to pipe: %d\n", i, i, i+1);
-                read_fd = i;
-                write_fd = i+1;
-            }
+            int read_fd = i-2;
+            int write_fd = i-1;
             struct apple the_apple;
             read(pipes[read_fd][READ], &the_apple, sizeof(the_apple));
-            printf("Node #%d (pid=%d) recieved the apple...\n", i+1, getpid());
-            //sleep(1); //TODO uncomment this
-            if(the_apple.destination != i-1) {
+            printf("Node #%d (pid=%d) recieved the apple...\n", i, getpid());
+            sleep(1); //TODO uncomment this
+            if(the_apple.destination != i) {
                 if(the_apple.destination == 0) {
                     printf("Messaged already recieved. Sending apple home...\n");
                 } else {
                     printf("Not the destination, passing it on...\n");
                 }
                 write(pipes[write_fd][WRITE], &the_apple, sizeof(the_apple));
-            } else if(the_apple.destination == i-1) {
-                if(the_apple.destination == 0) {
-                    printf("Apple returned home. We're done here.\n");
-                    
-                    exit(0);
-                }
-                printf("Received message: %s\n", the_apple.message);
+            } else if(the_apple.destination == i) {
+                printf("\033[0;32mReceived message: \033[0m%s\n", the_apple.message);
                 the_apple.destination = 0;
                 write(pipes[write_fd][WRITE], &the_apple, sizeof(the_apple));
+            }
+
+            if(i==n_processes) {
+                kill(parent_pid, SIGINT);
             }
 
             exit(0); // Don't keep the loop running in the child process
@@ -130,7 +117,7 @@ int main() {
     fgets(destination_process, 5, stdin);
     destination_process[strcspn(destination_process, "\n")] = 0;
 
-    if(!(the_apple.destination = atoi(destination_process) - 1)) { // We subtract 1 so that the nodes are not zero indexed for the user
+    if(!(the_apple.destination = atoi(destination_process))) {
         fail("Destination process must be a valid number", 1);
     }
     if(the_apple.destination > n_processes || the_apple.destination < 1) {
@@ -142,11 +129,14 @@ int main() {
     message[strcspn(message, "\n")] = 0;
     strcpy(the_apple.message, message);
 
+    printf("Node #1 (pid=%d) sending the apple off...\n", parent_pid);
     write(pipes[0][WRITE], &the_apple, sizeof(the_apple));
-
+    struct apple new_apple;
     while(!time_to_die) {
         pause();
     }
+    read(pipes[n_processes-1][READ], &new_apple, sizeof(the_apple));
+    printf("Got the apple back. Message: %s\n", new_apple.message);
 
     // Free the memory and get out of here
     printf("Cleaning up...\n");
@@ -158,7 +148,6 @@ int main() {
 // Gracefully handle ^C
 void sig_handler(int sig_num, siginfo_t *info, void *ucontext) {
     if(sig_num == SIGINT) {
-        printf(" received.  That's it, I'm shutting you down...\n");
         time_to_die = true;
     }
 }
