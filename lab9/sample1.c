b06e585d11eba5789be2bc0411d983c38c773a04
Hephaestos <thedanielfloyd@protonmail.com>
2022-03-21 09:23:59 -0400

    use getline instead of scanf

---
+++ lab9/sample1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 16
+
+int main() {
+    char *data1;
+    size_t len = 0;
+    ssize_t read;
+
+    data1 = malloc(SIZE);
+    printf("Please input username: ");
+    if((read = getline(&data1, &len, stdin)) != -1) {
+        printf("you entered: %s\n", data1);
+    }
+    if(ferror(stdin)) {
+        perror("Failed to read input\n");
+    }
+    free(data1);
+    return 0;
+}
