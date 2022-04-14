fe8f39f6a8f5efe732deecb92e8906b9c51ddbda
Hephaestos <thedanielfloyd@protonmail.com>
2022-04-14 14:25:41 -0400

    print out number of bytes

--- lab12/program2.c
+++ lab12/program2.c
@@ -4,16 +4,38 @@
 #include <sys/types.h>
 #include <errno.h>
 
-int main()
-{
-  DIR *dirPtr;
-  struct dirent *entryPtr;
+void humanize(long bytes, char* humanized) {
+    long out = bytes;
+    char unit = 'B';
+    if(bytes/1000 > 0) {
+        out /= 1000;
+        unit = 'K';
+        if(out/1000 > 0) {
+            out /= 1000;
+            unit = 'M';
+            if(out/1000 > 0) {
+                out /= 1000;
+                unit = 'G';
+            }
+        }
+    }
 
-  dirPtr = opendir (".");
+    sprintf(humanized,"%ld%c", out, unit);
+}
 
-  while ((entryPtr = readdir (dirPtr)))
-    printf ("%-20s\n", entryPtr->d_name);
+int main() {
+    DIR *dirPtr;
+    struct dirent *entryPtr;
 
-  closedir (dirPtr);
-  return 0;
+    dirPtr = opendir (".");
+
+
+    while ((entryPtr = readdir (dirPtr))) {
+        struct stat fileStat;
+        stat(entryPtr->d_name, &fileStat);
+        printf ("%ld: %-20s\n", fileStat.st_size, entryPtr->d_name);
+    }
+
+    closedir (dirPtr);
+    return 0;
 }
