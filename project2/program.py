adcb4a41cd2dac19544d9550f06415c80544b53e
Hephaestos <thedanielfloyd@protonmail.com>
2022-04-22 19:12:32 -0400

    Add first fit algo
    Co-authored-by: Nicholas R. Biesbrock <BIESBRON@mail.gvsu.edu>
    Co-authored-by: calebpoe00 <calebpoe00@users.noreply.github.com>

--- project2/program.py
+++ project2/program.py
@@ -1,14 +1,11 @@
-# very basic
-# right now, it only does 1 process at a time
-# need to figure out how to allocate new process every second
-
 import time
+MEMORY_SIZE = 50
 
 class Process:
     def __init__(self, num, size, runTime):
-        self.num = num # 1-30
-        self.size = size # 1-10
-        self.runTime = runTime # 1-5
+        self.num = num
+        self.size = size
+        self.runTime = runTime
 
     def __str__(self):
         return f'Process #:{self.num} Memory size:{self.size} Runtime remaining:{self.runTime}'
@@ -19,48 +16,42 @@ class Allocation:
         self.start = start
         self.end = end
 
-def firstFit(processes):
-    print("------First Fit------")
+    def __str__(self):
+        return f'Process #:{self.process.num} Runtime remaining:{self.process.runTime} Start:{self.start} End:{self.end}'
 
-    blockSize = 50
-    numProcesses = len(processes)
-    status = ["Unallocated"] * numProcesses
-
-    for i in range(numProcesses):
-
-        # check if block has enough free space
-        if blockSize >= processes[i].size:
-            status[i] = "Allocated"
-            # remove free space from block
-            blockSize -= processes[i].size
-            processStartTime = time.time()
-
-        while status[i] != "Freed":
-            processCurrentTime = time.time()
-            processElapsedTime = processCurrentTime - processStartTime
-
-            # terminate process if it completed its run time
-            if processElapsedTime >= processes[i].runTime:
-                status[i] = "Freed"
-                blockSize += processes[i].size
-      
-            # print out status every second
-            if processElapsedTime.is_integer():
-                print(status)
+def firstFit(process, allocations):
+    start = 0
+    end = 0
+    for allocation in allocations:
+        end = allocation.start
+        if end - start >= process.size:
+            allocations.append(Allocation(process, start, start + process.size))
+            return True
+        start = allocation.end
+    end = MEMORY_SIZE
+    if end - start >= process.size:
+        allocations.append(Allocation(process, start, start + process.size))
+        return True
+    return False
 
 def bestFit(nextProcess, allocations):
     allocations.append(Allocation(nextProcess, 0, 0))
-    return True
 
 def scheduler(processQueue, algorithm):
     allocations = []
     while(processQueue or allocations):
-        nextProcess = processQueue.pop(0)
-        algorithm(nextProcess, allocations)
         for allocation in allocations:
             allocation.process.runTime -= 1
             if allocation.process.runTime == 0:
                 allocations.remove(allocation)
+        if(processQueue):
+            nextProcess = processQueue.pop(0)
+            if not algorithm(nextProcess, allocations):
+                processQueue.insert(0,nextProcess)
+        for allocation in allocations:
+            print(allocation)
+        print("------------------------------------------")
+        time.sleep(1)
 
 if __name__ == '__main__':
     processes = [
@@ -69,4 +60,4 @@ if __name__ == '__main__':
         Process(3, 30, 2),
         Process(4, 25, 5)
     ]
-    scheduler(processes, bestFit)
+    scheduler(processes, firstFit)
