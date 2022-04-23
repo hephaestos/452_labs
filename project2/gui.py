172eb591999f01ae2dad3d2fe61c0a3018989453
Hephaestos <thedanielfloyd@protonmail.com>
2022-04-22 21:07:51 -0400

    refactor GUI  to class and add worst fit

--- project2/gui.py
+++ project2/gui.py
@@ -1,69 +1,64 @@
 from tkinter import *
 
-# generating GUI and canvas
-root = Tk()
-root.title('CIS 452 Dynamic Memory Allocation Project')
-# 16x9 aspect ratio
-root.geometry("1200x675")
-grey = '#bebebe'
-lightblue = '#ADD8E6'
-canvas = Canvas(root, width=1150, height=625, bg=grey)
-canvas.pack()
-width = 4
+class GUI:
+    def __init__(self, memsize):
+        self.memsize = memsize
+        # generating GUI and canvas
+        self.root = Tk()
+        self.root.title('CIS 452 Dynamic Memory Allocation Project')
+        # 16x9 aspect ratio
+        self.root.geometry("1200x675")
+        grey = '#bebebe'
+        lightblue = '#ADD8E6'
+        self.canvas = Canvas(self.root, width=1150, height=625, bg=grey)
+        self.canvas.pack()
+        self.width = 4
 
-def addToGUI(allocation, memsize, column):
-    malloc = allocation.start
-    size = allocation.process.size
-    num = allocation.process.num
-    column = column
+        self.canvas.create_rectangle(350, 85, 485, 585, fill="blue", width=self.width, outline="blue")
+        self.canvas.create_rectangle(620, 85, 755, 585, fill="blue", width=self.width, outline="blue")
+        self.canvas.create_rectangle(890, 85, 1025, 585, fill="blue", width=self.width, outline="blue")
 
-    canvas.create_rectangle(80 + (270*column), 85 + ((500/memsize) * malloc), 215 + (270*column), 85 + ((500/memsize) * (malloc + size)), fill='red', width=width)
-    printstring = "Proc " + str(num) + " Size " + str(size)
-    canvas.create_text(145 + (270*column), 95 + ((500/memsize) * malloc + size), text=printstring, fill="white", font=('Helvetica 13'))
+        # label for each bar
+        self.canvas.create_text(415, 605, text="First Fit", fill="black", font=('Helvetica 13'))
+        self.canvas.create_text(685, 605, text="Best Fit", fill="black", font=('Helvetica 13'))
+        self.canvas.create_text(955, 605, text="Worst Fit", fill="black", font=('Helvetica 13'))
 
-    root.update_idletasks()
-    root.update()
+        # left legend
+        self.canvas.create_rectangle(50, 85, 200, 435, fill="blue",width=self.width, outline="blue")
+        self.canvas.create_rectangle(55, 90, 195, 235, fill="white",width=self.width, outline="white")
+        self.canvas.create_text(120, 100, text="Next Process Size", fill="black", font=('Helvetica 11'))
+        self.canvas.create_text(109, 120, text="to be allocated:", fill="black", font=('Helvetica 11'))
+
+        self.canvas.create_text(89, 165, text="First: 50", fill="black", font=('Helvetica 12'))
+        self.canvas.create_text(90, 190, text="Best: 00", fill="black", font=('Helvetica 12'))
+        self.canvas.create_text(94, 215, text="Worst: 00", fill="black", font=('Helvetica 12'))
+
+        # title bar
+        self.canvas.create_text(575, 25, text="CIS 452 Dynamic Memory Allocation Project", fill="black", font=('Helvetica 15'))
+
+        self.root.update_idletasks()
+        self.root.update()
+
+    def addToGUI(self, allocation, column):
+        malloc = allocation.start
+        size = allocation.process.size
+        num = allocation.process.num
+        column = column
+
+        self.canvas.create_rectangle(80 + (270*column), 85 + ((500/self.memsize) * malloc), 215 + (270*column), 85 + ((500/self.memsize) * (malloc + size)), fill='red', width=self.width)
+        printstring = "Proc " + str(num) + " Size " + str(size)
+        self.canvas.create_text(145 + (270*column), 95 + ((500/self.memsize) * malloc + size), text=printstring, fill="white", font=('Helvetica 13'))
+
+        self.root.update_idletasks()
+        self.root.update()
 
 
-def removeFromGUI(allocation, memsize, column):
-    malloc = allocation.start
-    size = allocation.process.size
-    column = column
+    def removeFromGUI(self, allocation, column):
+        malloc = allocation.start
+        size = allocation.process.size
+        column = column
 
-    canvas.create_rectangle(80 + (270*column), 85 + ((500 / memsize) * malloc), 215 + (270*column), 85 + ((500 / memsize) * (malloc + size)), fill="blue", width=width, outline="blue")
+        self.canvas.create_rectangle(80 + (270*column), 85 + ((500 / self.memsize) * malloc), 215 + (270*column), 85 + ((500 / self.memsize) * (malloc + size)), fill="blue", width=self.width, outline="blue")
 
-    root.update_idletasks()
-    root.update()
-
-
-def InitGUI():
-    # my_canvas.create_rectangle(x1, y1, x2, y2 fill ="color")
-    # x1, y1: top left
-    # x2, y2: bottom right
-
-    # three empty bars
-    canvas.create_rectangle(350, 85, 485, 585, fill="blue", width=width, outline="blue")
-    canvas.create_rectangle(620, 85, 755, 585, fill="blue", width=width, outline="blue")
-    canvas.create_rectangle(890, 85, 1025, 585, fill="blue", width=width, outline="blue")
-
-    # label for each bar
-    canvas.create_text(415, 605, text="First Fit", fill="black", font=('Helvetica 13'))
-    canvas.create_text(685, 605, text="Best Fit", fill="black", font=('Helvetica 13'))
-    canvas.create_text(955, 605, text="Worst Fit", fill="black", font=('Helvetica 13'))
-
-    # left legend
-    canvas.create_rectangle(50, 85, 200, 435, fill="blue",width=width, outline="blue")
-    canvas.create_rectangle(55, 90, 195, 235, fill="white",width=width, outline="white")
-    canvas.create_text(120, 100, text="Next Process Size", fill="black", font=('Helvetica 11'))
-    canvas.create_text(109, 120, text="to be allocated:", fill="black", font=('Helvetica 11'))
-
-    canvas.create_text(89, 165, text="First: 50", fill="black", font=('Helvetica 12'))
-    canvas.create_text(90, 190, text="Best: 00", fill="black", font=('Helvetica 12'))
-    canvas.create_text(94, 215, text="Worst: 00", fill="black", font=('Helvetica 12'))
-
-    # title bar
-    canvas.create_text(575, 25, text="CIS 452 Dynamic Memory Allocation Project", fill="black", font=('Helvetica 15'))
-
-    root.update_idletasks()
-    root.update()
-
+        self.root.update_idletasks()
+        self.root.update()
--- project2/program.py
+++ project2/program.py
@@ -1,8 +1,9 @@
 import time
+import copy
 import asyncio
 from process import Process
 from allocation import Allocation
-import gui
+from gui import GUI
 MEMORY_SIZE = 100
 
 def firstFit(process, allocations):
@@ -29,48 +30,69 @@ def bestFit(process, allocations):
     for allocation in allocations:
         end = allocation.start
         if end - start >= process.size:
-            options.append(Allocation(process, start, start + process.size))
+            options.append(Allocation(process, start, end))
         start = allocation.end
     end = MEMORY_SIZE
     if end - start >= process.size:
-        options.append(Allocation(process, start, start + process.size))
+        options.append(Allocation(process, start, end))
     if options:
-        options.sort(key=lambda a: a.end - a.start)
+        options.sort(key=lambda a: (a.end - a.start))
+        options[0].end = options[0].start + process.size
         allocations.append(options[0])
         return True
     return False
 
-async def scheduler(processQueue, algorithm, name):
+def worstFit(process, allocations):
+    start = 0
+    end = 0
+    allocations.sort(key=lambda a: a.start)
+    options = []
+    for allocation in allocations:
+        end = allocation.start
+        if end - start >= process.size:
+            options.append(Allocation(process, start, end))
+        start = allocation.end
+    end = MEMORY_SIZE
+    if end - start >= process.size:
+        options.append(Allocation(process, start, end))
+    if options:
+        options.sort(key=lambda a: (a.end - a.start), reverse=True)
+        options[0].end = options[0].start + process.size
+        allocations.append(options[0])
+        return True
+    return False
+
+async def scheduler(processes, algorithm, gui, column):
+    processQueue = copy.deepcopy(processes)
     allocations = []
     while processQueue or allocations:
         for allocation in allocations:
             allocation.process.runTime -= 1
             if allocation.process.runTime == 0:
                 allocations.remove(allocation)
-                gui.removeFromGUI(allocation, MEMORY_SIZE, 1)
+                gui.removeFromGUI(allocation, column)
         if processQueue:
             nextProcess = processQueue.pop(0)
             if not algorithm(nextProcess, allocations):
                 processQueue.insert(0,nextProcess)
         for allocation in allocations:
             print(allocation)
-            gui.addToGUI(allocation, MEMORY_SIZE, 1)
+            gui.addToGUI(allocation, column)
         print("------------------------------------------")
         await asyncio.sleep(1)
 
 async def main():
     processes = [
-        Process(1, 40, 2),
+        Process(1, 70, 2),
         Process(2, 10, 5),
-        Process(3, 25, 3),
-        Process(4, 40, 4),
-        Process(5, 5, 4),
-        Process(6, 10, 2),
+        Process(3, 10, 5),
+        Process(4, 20, 3),
     ]
-    # gui.InitGUI()
+    g = GUI(MEMORY_SIZE)
     await asyncio.gather(
-        asyncio.create_task(scheduler(processes, firstFit, "ONE")),
-        asyncio.create_task(scheduler(processes, firstFit, "TWO")),
+        asyncio.create_task(scheduler(processes, firstFit, g, 1)),
+        asyncio.create_task(scheduler(processes, bestFit, g, 2)),
+        asyncio.create_task(scheduler(processes, worstFit, g, 3))
     )
 
 if __name__ == '__main__':
